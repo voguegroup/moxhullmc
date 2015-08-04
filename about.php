53e7366f28b679805d6d0ec430334b2d7a42f922
adamsuter <adam_suter@Adams-iMac.local>
2015-08-04 14:18:32 +0100

    New page

---
+++ about.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<!--[if lt IE 7 ]><html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]><html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]><html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]><html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="author" content="Moxhull Motor Company">
+<meta name="keywords" content="car finance birmingham, car finance sutton coldfield, car, finance, sutton coldfield, used car finance sutton coldfield, car finance, used fiesta finance, used corsa finance, second hand cars finance sutton coldfield, used fords sutton coldfield, used vaxhaull sutton coldfield, used vw sutton coldfield, used renault sutton coldfield">
+<meta name="description" content="We are a small team of highly experienced motor trade professionals with 30 years of industry knowledge striving to provide a personalised, valued service to all of our customers.">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>Moxhull Motor Company | Sutton Coldfield Used Car Sales Birmingham</title>
+<link href='http://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+<link rel="shortcut icon" href="favicon.ico">
+
+<link href="style.css" media="screen" rel="stylesheet">
+<link href="screen.css" media="screen" rel="stylesheet">
+<!-- custom CSS -->
+<link href="custom.css" media="screen" rel="stylesheet">
+
+<!-- main JS libs  -->
+<script src="js/libs/modernizr.min.js"></script>
+<script src="js/libs/respond.min.js"></script>					 
+<script src="js/libs/jquery.min.js"></script>
+
+<!-- scripts  -->
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/general.js"></script>
+<script src="js/hoverIntent.js"></script>
+<!-- carousel -->
+<script src="js/jquery.carouFredSel.min.js"></script>
+<script src="js/jquery.touchSwipe.min.js"></script>
+<!-- styled select -->
+<link rel="stylesheet" href="css/cusel.css">
+<script src="js/cusel-min.js"></script>
+<!-- custom input -->
+<script src="js/jquery.customInput.js"></script>
+<!-- range slider -->
+<link href="css/jslider.css" rel="stylesheet">
+<script type="text/javascript" src="js/jquery.slider.bundle.js"></script>
+
+</head>
+
+<body>
+<div class="body_wrap">
+	
+	<!-- header top bar -->
+	<?php include ('components/header.php'); ?>
+	<!--/ header top bar -->
+		
+<!-- header -->
+<div class="header header_thin" style="background-image:url(images/temp/slider_1_2.jpg)">
+            
+	<div class="header_title">
+    	&nbsp;
+    </div>
+
+</div>
+<!--/ header -->
+
+
+
+<!-- breadcrumbs -->
+<div class="middle_row row_white breadcrumbs">
+    <div class="container">
+        <p><a href="/">Home</a>  <span class="separator">&rsaquo;</span>  <a href="#">About Us</a></p>
+        
+    </div>
+</div>
+<!--/ breadcrumbs -->
+
+<!-- middle -->   
+<div id="middle" class="cols2 full_width">
+	<div class="container clearfix">  
+    	
+       
+		<!-- content -->
+        <div class="content">
+        <!-- car types -->
+	<div class="middle_row  search_row contact_form">
+		<div class="container">  
+			
+           
+           <h1>Moxhull Motor Company</h1>
+           <p>We are a small team of highly experienced motor trade professionals with 30 years of industry knowledge. Our main offering is to provide a personalised, valued service to all of our customers.</p>
+           <p>All of our used cars are HPI approved and come with 3 months warranty. Most are availale to drive away the same day.<br>
+<br>
+If you require used car finance we are able to offer competitive options that match your circumstances.<br>
+<br>
+We also accept all major debit and credit cards.<br>
+<br>
+<img src="images/temp/hpi.png" alt="HPI car check approved"><img style="margin-left:20px" src="images/temp/warranty.png" alt="3 month warranty"><img style="margin-left:20px" src="images/temp/cards.png" alt="accepts most debit and credit cards"></p>
+           
+             
+            
+              <div class="text_box">
+            	<p>
+                <a href="contact.php" class="btn btn_big btn_white"><span>APPLY FOR FINANCE <strong>0121 351 3311</strong></span></a>
+                <a href="cars.php" class="btn btn_big btn_orange"><span>VIEW OUR STOCK</span></a>
+                </p>
+            	
+            </div> 
+		</div>
+	</div>
+    <!--/ car types -->	
+            
+            
+            
+            
+            
+        </div>
+        <!--/ content -->
+        
+        
+              
+    </div>
+</div>
+<!--/ middle -->
+
+
+
+ <?php include ('components/brands.php'); ?>
+    
+<!--/ middle -->
+
+<?php include ('components/footer.php'); ?>
+
+</div>
+</body>
+</html>

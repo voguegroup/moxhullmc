8010507feda30bbb315166408eadf614298fa8ed
adamsuter <adam_suter@Adams-iMac.local>
2015-08-04 14:19:26 +0100

    new footer style

--- components/footer.php
+++ components/footer.php
@@ -1,74 +1,34 @@
 <div class="footer">
 	<div class="container clearfix">
     
-		<div class="f_col f_col_1">  
-            <h3>Vehicles:</h3>
-            <ul>
-                <li><a href="#"><span>Motorbikes</span></a></li> 
-                <li><a href="#"><span>Compacts</span></a></li>
-                <li><a href="#"><span>Sedans</span></a></li>
-                <li><a href="#"><span>4x4 SUVs</span></a></li>
-                <li><a href="#"><span>Pickups</span></a></li>
-                <li><a href="#"><span>Vans & Trucks</span></a></li>                                
-            </ul>
-        </div>
-        <!--/ footer col 1 -->
+		
+       
         
-        <div class="f_col f_col_2">        	
-			<h3>Services:</h3>
-            <ul>
-                <li><a href="#"><span>Buy a car</span></a></li> 
-                <li><a href="#"><span>Sell your Car</span></a></li>
-                <li><a href="#"><span>Buy Back</span></a></li>
-                <li><a href="#"><span>Repair Shop </span></a></li>
-            </ul>
-        </div>
-        <!--/ footer col 2 -->
+     
+      
+    
         
-        <div class="f_col f_col_3">        	
-            <h3>Privacy:</h3>
-            <ul>
-                <li><a href="#"><span>Terms & Conditions</span></a></li> 
-                <li><a href="#"><span>Privacy Statement</span></a></li>
-                <li><a href="#"><span>F.A.Q.</span></a></li>
-                <li><a href="#"><span>Support</span></a></li>
-                <li><a href="#"><span>Confidentiality</span></a></li>
-            </ul>
-        </div>        
-        <!--/ footer col 3 -->
         
-        <div class="f_col f_col_4">   
-           	<h3>OUR SHOWROOM</h3>
-            <div class="footer_address">
-                <div class="address">
-	                Address Line 1<br>
-	                Wishaw, Sutton Coldfield B76
-                </div>
-                <div class="hours">
-                	Mon - Fri: 10AM - 6 PM<br>
-					Sat - Sun: 10AM - 4 PM
-				</div>
-                <a href="contact.php" class="notice">Get directions</a>
-            </div>
-            
-      	</div>
-        <!--/ footer col 4 -->
-        
-        <div class="clear"></div>
-        
-        <div class="footer_social">
-        	<div class="social_inner">
-			    
-			    <a href="#" class="social-fb"><span>Facebook</span></a>
-			    <a href="#" class="social-twitter"><span>Twitter</span></a>
-			    
-			    <a href="#" class="social-linkedin"><span>LinkedIn</span></a>
-			   
-            </div>
-		</div>
         
         <div class="footer_contacts">
-        	<span class="phone">0121 123 4567</span>
+        
+        	<h3>OUR SHOWROOM</h3>
+            Unit 1 Bricklyn Farm, 
+Bulls Lane, 
+Wishaw 
+B76 9QN
+        </div>
+        
+        <div class="copyright">
+        <img src="images/temp/hpi.png" width="43" height="43" alt="HPI car check approved"><img style="margin-left:20px" width="43" height="43" src="images/temp/warranty.png" alt="3 month warranty"><img style="margin-left:20px" width="43" height="43" src="images/temp/cards.png" alt="accepts most debit and credit cards"> 
+        </div>
+        
+      
+        <div class="clear" style="margin-top:20px"></div>
+        
+        <div class="footer_contacts">
+        
+        	<span class="phone">0121 351 3311</span>
             <span class="email">sales@moxhullmotorcompany.co.uk</span>
         </div>
         

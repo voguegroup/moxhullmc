4628023cd44be8c3c9884555be9ce9d6f4d6469c
Tom Ford <thomasanthonyford@gmail.com>
2015-08-05 14:23:38 +0100

    Fix log script

--- admin/components/log-script.php
+++ admin/components/log-script.php
@@ -3,21 +3,30 @@
 include (dirname(__FILE__) . '/../config/config.php');
 
 if (isset($_GET['erun'])) {
-	include($cms_root_url . '/components/mysql_connect.inc');
-	$query = "INSERT INTO fsclog SET uid='{$_SESSION['uid']}', sesip='{$_SERVER['REMOTE_ADDR']}', action='{$_GET['erun']}', action_time=NOW()";
-	$query_result = mysql_query($query);
+	$sql=$dbo->prepare("INSERT INTO fsclog SET uid= :uid , sesip= :sesip, action= :action', action_time= :action_time");
+$sql->execute(array(
+":uid" => $_SESSION['uid'],
+":sesip" => $_SERVER['REMOTE_ADDR'],
+":action" =>	 $_GET['erun'],
+":action_time" => gmdate("Y-m-d H:i:s")
+));
+
 }
 
 if (isset($_GET['srun'])) {
 	$srun = $_GET['srun'];
 } 
-elseif(isset($srun)) {
+else if(isset($srun)) {
 	$srun = $srun;
 }
 if($srun) {
-	include($cms_root_url . '/components/mysql_connect.inc');
-	$query = "INSERT INTO fsclog SET uid='{$_SESSION['uid']}', sesip='{$_SERVER['REMOTE_ADDR']}', action='$srun', action_time=NOW()";
-	$query_result = mysql_query($query);
+	$sql=$dbo->prepare("INSERT INTO fsclog SET uid= :uid , sesip= :sesip, action= :action', action_time= :action_time");
+$sql->execute(array(
+":uid" => $_SESSION['uid'],
+":sesip" => $_SERVER['REMOTE_ADDR'],
+":action" =>	 $_GET['srun'],
+":action_time" => gmdate("Y-m-d H:i:s")
+));
 }
 
 ?>

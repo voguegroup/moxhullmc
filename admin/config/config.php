e6fe4ab9ec7161813a16ba700c6ba86764060d6d
Tom Ford <thomasanthonyford@gmail.com>
2015-08-05 11:52:48 +0100

    New mysql_connect and config files

--- admin/config/config.php
+++ admin/config/config.php
@@ -16,11 +16,11 @@ $cms_abs_url = "http://moxhullmc.wearefundamental.com/admin"; // e.g. 'http://ww
 $cms_root_url = "/mnt/stor12-wc2-dfw1/589243/moxhullmc.wearefundamental.com/web/content/admin"; // e.g. '/home/username/public_html/admin'
 // Database details
 
-$user = 'root';
-$password = 'etuRenun4';
-$db = 'inventory';
-$host = 'localhost';
-$port = 8889;
+$user = '589243_mox';
+$password = 'TT9-SLQ-VtT-zkp';
+$db = '589243_mox';
+$host = 'mysql51-011.wc2.dfw1.stabletransit.com';
+$port = 21;
 
 $link = mysqli_init();
 $success = mysqli_real_connect(
@@ -32,8 +32,8 @@ $success = mysqli_real_connect(
    $port
 );
 
-$db_host = 'mysql51-011.wc2.dfw1.stabletransit.com'; // e.g. 'localhost'
-$db_user = '589243_mox'; // MySQL username
-$db_pass = 'TT9-SLQ-VtT-zkp'; // MySQL password
-$db_name = '589243_mox'; // MySQL database name
+//$db_host = 'mysql51-011.wc2.dfw1.stabletransit.com'; // e.g. 'localhost'
+//$db_user = '589243_mox'; // MySQL username
+//$db_pass = 'TT9-SLQ-VtT-zkp'; // MySQL password
+//$db_name = '589243_mox'; // MySQL database name
 ?>
